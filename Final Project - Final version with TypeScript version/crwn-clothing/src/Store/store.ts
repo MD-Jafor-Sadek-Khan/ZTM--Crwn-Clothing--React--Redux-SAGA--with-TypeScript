9ed65af97d1a4eace379d0645c62752b53e854a1
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-04-26 20:58:43 +0600

    typed all the middlewares

--- Final Project - Final version with TypeScript version/crwn-clothing/src/Store/store.ts
+++ Final Project - Final version with TypeScript version/crwn-clothing/src/Store/store.ts
@@ -1,7 +1,7 @@
 import { compose, createStore, applyMiddleware, Middleware } from "redux"
 import logger from "redux-logger"
 import { rootReducer } from "./root-reducer"
-import { persistStore, persistReducer } from "redux-persist"
+import { persistStore, persistReducer, PersistConfig } from "redux-persist"
 import storage from "redux-persist/lib/storage"
 import { rootSaga } from "./root-saga"
 import createSagaMiddleware from "redux-saga"
@@ -29,7 +29,10 @@ const composeEnhancer =
 
 const composeEnhancers = composeEnhancer(applyMiddleware(...middlewares))
 
-const persistConfig = {
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[]
+}
+const persistConfig: ExtendedPersistConfig = {
   key: "root",
   storage,
   whitelist: ["cart"],
