3166ba9e7c4b38749b7152b2a3c0b4d00b63fd37
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-04-28 10:30:39 +0600

    styled cart-items

--- src/Components/Cart-Item/cart-item.styles.jsx
+++ src/Components/Cart-Item/cart-item.styles.jsx
@@ -5,12 +5,18 @@ export const CartItemContainer = styled.div`
   display: flex;
   height: 80px;
   margin-bottom: 15px;
-  border-top:2px solid white;
-  border-bottom:2px solid white;
+  border-top: 2px solid white;
+  border-bottom: 2px solid white;
   img {
     width: 30%;
   }
-  
+  transition: transform 0.3s ease; 
+
+  &:hover {
+    opacity: 0.8;
+    box-shadow: -10px 10px 10px rgba(0, 0, 0, 0.6);
+    transform: scale(1.08); 
+  }
 `
 export const ItemDetails = styled.div`
   width: 70%;
